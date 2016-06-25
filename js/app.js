957caa2423936cbe05681566f2fc04c351848ef4
lynnerw <larwitter@gmail.com>
2016-06-24 22:55:25 -0400

    made some progress - not done yet

--- js/app.js
+++ js/app.js
@@ -15,34 +15,44 @@ $(document).ready(function(){
 		// generate a number between 1-100
 		function getRandomNum(min, max) {
 			secretNum = Math.floor(Math.random() * (max - min) + min);
-			// console.log('The secret number inside the function is ' + secretNum);
+		  // console.log('The secret number inside the function is ' + secretNum);
 			return secretNum;
-};
-var secretNum = getRandomNum(1, 100);
-// console.log('The secret number outside the function is ' + secretNum);
+		};
 
-		// compare user input with system-generated number
-		var userNum = prompt('Please enter a whole number greater than zero and less than 100.', 90);
+		// user input: each time the user guesses, test the guess against the random number and increase the count
+		var count = 0;
+		$('input[type="submit"]').click(function() {
+				var textInput = $('input[name="userGuess"]');
+				var userNum = textInput.val();
+					// console.log('userNum is ' + userNum);
 
-		// prompt user for an integer
-		while (isNaN(parseInt(userNum))) {
-		  document.write('Ooops. I need a whole number. Try again...' + '<br>');
-		  var userNum = prompt('Please enter a whole number greater than zero and less than 100.', 90);
-		  }
-			// check whether the user guessed the secret number value
-		var userNum = parseInt(userNum);
-		if ((parseInt(userNum) % parseInt(userNum)) === 0) {
-						console.log('User picked ' + userNum + '; the secret number is ' + secretNum);
-		        document.write('You picked ' + (parseInt(userNum)) + '.' + '<br>');
-						if (userNum == secretNum) {
-							document.write('Awesome! You guessed the secret number!')
-						} // checkw whether the user's guess was too high or too low
-						else if (parseInt(userNum) > secretNum) {
-							document.write('Hmmm. That\'s a little high.')
-						} else {
-							document.write('Ooops; you need to bump that number up a bit.')
-						}
+				if (count <= 5) {
+							var secretNum = getRandomNum(1, 100);
+								// console.log('The secret number outside the function is ' + secretNum);
+								// check whether the user guessed the secret number value
+							console.log('User picked ' + userNum + '; the secret number is ' + secretNum);
+							$('#guessList ul').html('You picked ' + userNum + '.' + '<br>');
+				  		if (userNum > secretNum) {
+									$('#guessList ul').html('Hmmm. Your guess was a little too high.');
+								}
+								else {
+									$('#guessList ul').html('Hmmm. Your guess was a little too low.');
+								}
+							count++;
+							// console.log('count is ' + count);
+							$('#count').html(count);
+				}
+				else {
+					$('#question').append('<br>' + ' Don\'t you have something better to do?');
+				}
 
-		      };
+			// clear the input
+			textInput.val('');
+
+		});
+
+// check that user input is an integer less than 100
+//if (isNaN(parseInt(userNum))) {
+//}
 
 });
